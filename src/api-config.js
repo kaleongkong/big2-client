50a1021ce230fa81c22f84f623f5501feab27ea0
kaleong kong <kaleongkong@yahoo.com>
2018-10-20 11:10:29 -0700

    fix prod

--- src/api-config.js
+++ src/api-config.js
@@ -4,7 +4,7 @@ let SERVER_HOST = process.env.REACT_APP_BACKEND_HOST || '';
 let WEBSOCKET_HOST = process.env.REACT_APP_BACKEND_HOST || 'ws://192.168.86.134:5000';
 if(hostname === 'funbig2.herokuapp.com') {
   SERVER_HOST = 'https://funbig2apis.herokuapp.com';
-  WEBSOCKET_HOST = 'ws://funbig2apis.herokuapp.com';
+  WEBSOCKET_HOST = 'wss://funbig2apis.herokuapp.com';
 } else if(hostname === 'staging.realsite.com') {
 } else if(/^qa/.test(hostname)) {
 }
