26789fc28dcea9b89711d6ccc970afd6218aa203
kaleong kong <kaleongkong@yahoo.com>
2018-11-23 09:39:25 -0800

    minor update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import GameFrame from './game_frame/game-frame';
-import Card from './item/card';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<GameFrame />, document.getElementById('root'));
